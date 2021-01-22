6af1e55de29e244ec1a58d91ac62cb364114810f
Zhengqian Kuang <dev.kuang@gmail.com>
2021-01-22 08:09:12 -0600

    in progress

--- mylab/components/hooks-demo/table-of-contents/index.tsx
+++ mylab/components/hooks-demo/table-of-contents/index.tsx
@@ -1,12 +1,62 @@
 import React, { useLayoutEffect } from 'react';
-import { View } from "react-native";
+import { SectionList, SectionListData, View, Text } from "react-native";
 import { styles as commonStyles } from '../../../../components/common/style';
 import { getUIHierarchy } from '../../../resources/hierarchy';
 import { styles } from "./style";
 
+type TableOfContentsItem = {
+  index: number,
+  title: string,
+};
+type TableOfContentsSection = {
+  title: string,
+  data: TableOfContentsItem[],
+};
+
+const tableOfContents: TableOfContentsSection[] = [
+  {
+    title: 'Hooks',
+    data: [
+      {
+        index: 0,
+        title: 'useCallback',
+      },
+      {
+        index: 1,
+        title: 'useContext',
+      },
+      {
+        index: 2,
+        title: 'useEffect',
+      },
+      {
+        index: 3,
+        title: 'useLayoutEffect',
+      },
+      {
+        index: 4,
+        title: 'useRef',
+      },
+      {
+        index: 5,
+        title: 'useState',
+      },
+    ],
+  },
+];
+
 const HooksDemoTableOfContentsView = () => {
   return (
-    <View style={styles.baseView} />
+    <View style={styles.container}>
+      <SectionList
+        sections={tableOfContents}
+        renderItem={({item}) => {
+          return (<Text>{item.title}</Text>)
+        }}
+        renderSectionHeader={({section}) => <Text>{section.title}</Text>}
+        keyExtractor={(item, index) => `${index}`}
+      />
+    </View>
   );
 };
 
--- mylab/components/hooks-demo/table-of-contents/style.ts
+++ mylab/components/hooks-demo/table-of-contents/style.ts
@@ -8,6 +8,24 @@ import {
       height: '100%',
       // backgroundColor: 'cyan',
     },
+    container: {
+      flex: 1,
+      paddingTop: 0
+    },
+    sectionHeader: {
+      paddingTop: 2,
+      paddingLeft: 10,
+      paddingRight: 10,
+      paddingBottom: 2,
+      fontSize: 14,
+      fontWeight: 'bold',
+      backgroundColor: 'rgba(247,247,247,1.0)',
+    },
+    item: {
+      padding: 10,
+      fontSize: 18,
+      height: 44,
+    },
   });
   
   export {
